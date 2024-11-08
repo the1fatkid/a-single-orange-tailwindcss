821cc46588a3e8ff445c798b6720c7cca2a64d9c
Shivika <shivikabahri@gmail.com>
2024-11-08 21:50:57 +0530

    Add source files

---
+++ README.md
@@ -0,0 +1,1 @@
+# a-single-orange-tailwindcss
---
+++ config.js
@@ -0,0 +1,20 @@
+tailwind.config= {
+    theme:{
+        extend:{
+            colors:{
+                pale_orange: "#ffd9a6",
+                light_orange: "#fbb03b",
+                orange: "#f7931e"
+            },
+            fontFamily:{
+                playfair: ['Playfair Display SC', 'serif'],
+                sans: ['Josefin Sans', 'sans-serif']
+            },
+            backgroundImage:{
+                mobile: "url(assets/bg-mobile.png)",
+                desktop: "url(assets/bg-desktop.png)"
+            }
+
+        }
+    }
+}
